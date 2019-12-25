22c4b0c21c17900c5a78df816262cc2d335c87f8
John <doanja@plu.edu>
2019-12-25 14:20:24 -0800

    updated login form on index.html, updated nav.js

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -5,36 +5,47 @@
  * @param {string} parentElement the parent element's name to append this to
  */
 const renderSettingsLink = (page, userId, parentElement) => {
-  const li = $("<li>");
-  const a = $("<a>", { href: `/${page}/${userId}` });
-  const icon = $("<i>", { class: "large material-icons", id: "profile-icon" }).text(
-    "account_circle"
-  );
+  const li = $('<li>');
+  const a = $('<a>', { href: `/${page}/${userId}` });
+  const icon = $('<i>', {
+    class: 'large material-icons',
+    id: 'profile-icon'
+  }).text('account_circle');
 
   $(parentElement).append(li);
   li.append(a);
   a.append(icon);
 };
 
+const renderDivider = parentElement => {
+  const divider = $('<div>', { class: 'dropdown-divider' });
+
+  $(parentElement).append(divider);
+};
+
 /**
  * function to render nav links
- * @param {string} title the text to show on the link
- * @param {string} page the html page name
+ * @param {string} linkTitle the text to show on the link
+ * @param {string} pageName the html page name
  * @param {number} userId the user's id
  * @param {string} parentElement the class or id name to append this to
  */
-const renderNavLinks = (title, page, userId, parentElement) => {
-  const li = $("<li>", { class: "tab" });
-  const a = $("<a>", { href: `/${page}/${userId}` }).text(title);
-  $(parentElement).append(li);
-  li.append(a);
+const renderNavLinks = (linkTitle, pageName, userId, parentElement) => {
+  const a = $('<a>', {
+    class: 'dropdown-item',
+    href: `/${pageName}/${userId}`
+  }).text(linkTitle);
+  $(parentElement).append(a);
 };
 
 $(document).ready(() => {
-  const userId = window.location.href.split("/")[window.location.href.split("/").length - 1];
+  const userId = window.location.href.split('/')[
+    window.location.href.split('/').length - 1
+  ];
 
-  renderNavLinks("Home", "dashboard", userId, ".tabs");
-  renderNavLinks("Expenses", "expenses", userId, ".tabs");
-  renderNavLinks("Budget", "budget", userId, ".tabs");
-  renderSettingsLink("profile", userId, ".hide-on-med-and-down");
+  renderNavLinks('Dashboard', 'dashboard', userId, '#menu');
+  renderNavLinks('Overview', 'overview', userId, '#menu');
+  renderNavLinks('Expenses', 'expenses', userId, '#menu');
+  renderDivider('#menu');
+  renderNavLinks('Profile', 'profile', userId, '#menu');
 });
