304e9fe74a2c122efd3bee0fbab7d83bcb1db51f
John <doanja@plu.edu>
2019-12-28 17:52:39 -0800

    login page now shows error if incorrect email or password was entered

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,11 @@ module.exports = (app, path, passport) => {
     res.sendFile(path.join(__dirname, '../public/html/login.html'));
   });
 
+  // used for login failures
+  app.get('/login/err', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/html/login-failed.html'));
+  });
+
   app.get('/logout', (req, res) => {
     req.session.destroy(err => {
       res.redirect('/');
@@ -29,8 +34,21 @@ module.exports = (app, path, passport) => {
     })
   );
 
-  app.post('/login', passport.authenticate('local-login'), (req, res) => {
-    res.redirect('/dashboard/' + req.user.id);
+  app.post('/login', (req, res, next) => {
+    passport.authenticate('local-login', (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.redirect('/login/err');
+      }
+      req.login(user, err => {
+        if (err) {
+          return next(err);
+        }
+        return res.redirect('/dashboard/' + user.id);
+      });
+    })(req, res, next);
   });
 
   app.get('/logout', (req, res) => {
