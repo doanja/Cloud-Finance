448eeaeab27c0efdf61acf14145fa6eaa00ca902
John <doanja@plu.edu>
2020-01-01 01:15:59 -0800

    able to parse csv file

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,27 +1,11 @@
-const helloWorld = () => {
-  console.log('hello world');
-};
-
 const parseFile = () => {
-  console.log('aprse');
-  $('#files').parse({
-    config: {
-      delimiter: 'auto',
-      complete: helloWorld
-    },
-    before: function(file, inputElem) {
-      // executed before parsing each file begins;
-      // what you return here controls the flow
-      console.log('Parsing file...', file);
-    },
-    error: function(err, file, inputElem, reason) {
-      // executed if an error occurs while loading the file,
-      // or if before callback aborted for some reason
-      console.log('ERROR:', err, file);
-    },
-    complete: function() {
-      // executed after all files are complete
-      console.log('Done with all files');
+  const file = $('#files')[0].files[0];
+
+  Papa.parse(file, {
+    download: true,
+    complete: function(results) {
+      const { data } = results;
+      console.log('data :', data);
     }
   });
 };
