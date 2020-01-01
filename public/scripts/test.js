be924e06310bca82c48e92c6e731c024281ffc70
John <doanja@plu.edu>
2020-01-01 00:44:09 -0800

    updated html, and parse function

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,19 +1,27 @@
+const helloWorld = () => {
+  console.log('hello world');
+};
+
 const parseFile = () => {
   console.log('aprse');
-  $('input[type=file]').parse({
+  $('#files').parse({
     config: {
-      // base config to use for each file
+      delimiter: 'auto',
+      complete: helloWorld
     },
     before: function(file, inputElem) {
       // executed before parsing each file begins;
       // what you return here controls the flow
+      console.log('Parsing file...', file);
     },
     error: function(err, file, inputElem, reason) {
       // executed if an error occurs while loading the file,
       // or if before callback aborted for some reason
+      console.log('ERROR:', err, file);
     },
     complete: function() {
       // executed after all files are complete
+      console.log('Done with all files');
     }
   });
 };
