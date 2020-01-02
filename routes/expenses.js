1af284cd35b16b14c529951c4e06e06da274f3bc
John <doanja@plu.edu>
2020-01-01 18:29:49 -0800

    added check for "N/A" category before creating new expenses from CSV

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,16 +1,4 @@
 module.exports = (app, db, joi) => {
-  // get all expenses belonging to the category
-  app.get('/api/expense/:id', (req, res) => {
-    db.Expense.findAll({ where: { CategoryId: req.params.id } })
-      .then(data => {
-        res.status(200).json(data);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json({ error: err });
-      });
-  });
-
   // create a single expense
   app.post('/api/expense/', (req, res) => {
     const { amount, description, date, CategoryId } = req.body;
@@ -58,37 +46,104 @@ module.exports = (app, db, joi) => {
       });
   });
 
+  // post route for bulk creating expenses from a csv file
   app.post('/api/expense/csv', (req, res) => {
     const { id, data } = req.body;
 
-    // create a new category (temp category for the new rows)
-    db.Category.create({
-      name: 'N/A',
-      goal: 0,
-      UserId: id
+    // check to see if N/A category already exists
+    db.Category.findOne({
+      where: {
+        [db.Op.and]: [{ name: 'N/A' }, { UserId: id }]
+        // name: 'N/A'
+        // // UserId: id
+      }
     })
-      .then(newCategory => {
-        // get the new CategoryId
-        const CategoryId = newCategory.id;
+      .then(category => {
+        // 'N/A' category was found
+        if (category) {
+          console.log('CATEGORY FOUND ###################################');
+          // get the new CategoryId
+          const CategoryId = category.id;
 
-        // add the CategoryId to each row
-        data.forEach(row => {
-          row.CategoryId = CategoryId;
-        });
-
-        // create expense using the csv
-        db.Expense.bulkCreate(data)
-          .then(newExpense => {
-            res.status(200).json(newExpense);
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(400).json({ error: err });
+          // add the CategoryId to each row
+          data.forEach(row => {
+            row.CategoryId = CategoryId;
+            console.log('row :', row);
           });
+
+          // create expense using the csv
+          db.Expense.bulkCreate(data)
+            .then(newExpense => {
+              res.status(200).json(newExpense);
+            })
+            .catch(err => {
+              console.log(err);
+              res.status(400).json({ error: err });
+            });
+        }
+        // 'N/A' category was not found, so one is created
+        else {
+          console.log('CATEGORY NOT FOUND ###################################');
+          console.log('id:', id);
+          // use sequelize to create the new user passing in the newUser object
+          db.Category.create({
+            name: 'N/A',
+            goal: 0,
+            UserId: id
+          }).then(newCategory => {
+            // get the new CategoryId
+            const CategoryId = newCategory.id;
+
+            // add the CategoryId to each row
+            data.forEach(row => {
+              row.CategoryId = CategoryId;
+              console.log('row :', row);
+            });
+
+            // create expense using the csv
+            db.Expense.bulkCreate(data)
+              .then(newExpense => {
+                res.status(200).json(newExpense);
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(400).json({ error: err });
+              });
+          });
+        }
       })
       .catch(err => {
         res.status(400).json({ error: err });
       });
+
+    // // create a new category (temp category for the new rows)
+    // db.Category.create({
+    //   name: 'N/A',
+    //   goal: 0,
+    //   UserId: id
+    // })
+    //   .then(newCategory => {
+    //     // get the new CategoryId
+    //     const CategoryId = newCategory.id;
+
+    //     // add the CategoryId to each row
+    //     data.forEach(row => {
+    //       row.CategoryId = CategoryId;
+    //     });
+
+    //     //   // create expense using the csv
+    //     //   db.Expense.bulkCreate(data)
+    //     //     .then(newExpense => {
+    //     //       res.status(200).json(newExpense);
+    //     //     })
+    //     //     .catch(err => {
+    //     //       console.log(err);
+    //     //       res.status(400).json({ error: err });
+    //     //     });
+    //   })
+    //   .catch(err => {
+    //     res.status(400).json({ error: err });
+    //   });
   });
 
   // update a single Expense
