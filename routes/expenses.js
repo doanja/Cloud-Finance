e2c269deee8c7e3aef17689b5230672c7b1c5fbe
John <doanja@plu.edu>
2020-01-01 18:34:09 -0800

    updated comments and removed console logs

--- routes/expenses.js
+++ routes/expenses.js
@@ -50,25 +50,19 @@ module.exports = (app, db, joi) => {
   app.post('/api/expense/csv', (req, res) => {
     const { id, data } = req.body;
 
-    // check to see if N/A category already exists
+    // check to see if "N/A" category already exists
     db.Category.findOne({
-      where: {
-        [db.Op.and]: [{ name: 'N/A' }, { UserId: id }]
-        // name: 'N/A'
-        // // UserId: id
-      }
+      where: [{ name: 'N/A' }, { UserId: id }]
     })
       .then(category => {
-        // 'N/A' category was found
+        // "N/A" category was found
         if (category) {
-          console.log('CATEGORY FOUND ###################################');
           // get the new CategoryId
           const CategoryId = category.id;
 
           // add the CategoryId to each row
           data.forEach(row => {
             row.CategoryId = CategoryId;
-            console.log('row :', row);
           });
 
           // create expense using the csv
@@ -81,11 +75,10 @@ module.exports = (app, db, joi) => {
               res.status(400).json({ error: err });
             });
         }
-        // 'N/A' category was not found, so one is created
+
+        // "N/A" category was not found, so one is created
         else {
-          console.log('CATEGORY NOT FOUND ###################################');
-          console.log('id:', id);
-          // use sequelize to create the new user passing in the newUser object
+          // create the "N/A" category
           db.Category.create({
             name: 'N/A',
             goal: 0,
@@ -97,7 +90,6 @@ module.exports = (app, db, joi) => {
             // add the CategoryId to each row
             data.forEach(row => {
               row.CategoryId = CategoryId;
-              console.log('row :', row);
             });
 
             // create expense using the csv
@@ -115,35 +107,6 @@ module.exports = (app, db, joi) => {
       .catch(err => {
         res.status(400).json({ error: err });
       });
-
-    // // create a new category (temp category for the new rows)
-    // db.Category.create({
-    //   name: 'N/A',
-    //   goal: 0,
-    //   UserId: id
-    // })
-    //   .then(newCategory => {
-    //     // get the new CategoryId
-    //     const CategoryId = newCategory.id;
-
-    //     // add the CategoryId to each row
-    //     data.forEach(row => {
-    //       row.CategoryId = CategoryId;
-    //     });
-
-    //     //   // create expense using the csv
-    //     //   db.Expense.bulkCreate(data)
-    //     //     .then(newExpense => {
-    //     //       res.status(200).json(newExpense);
-    //     //     })
-    //     //     .catch(err => {
-    //     //       console.log(err);
-    //     //       res.status(400).json({ error: err });
-    //     //     });
-    //   })
-    //   .catch(err => {
-    //     res.status(400).json({ error: err });
-    //   });
   });
 
   // update a single Expense
